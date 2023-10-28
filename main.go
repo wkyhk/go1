c603b0e4d62d9086448a0bb6b4c0f53abaa97f09
WHK <841065531@qq.com>
2023-10-28 19:36:14 +0800

    1

--- main.go
+++ main.go
@@ -1,32 +1,5 @@
 package main
 
-import "GO1/leetcode"
-
 func main() {
-	leetcode.TestmamatrixSum()
-}
 
-/* func main() {
-	start := time.Now()
-	var wg sync.WaitGroup
-	ans := make([]int, 0)
-	for i := 1; i < 65535; i++ {
-		wg.Add(1)
-		go func(j int) {
-			defer wg.Done()
-			add := fmt.Sprintf("localhost:%d", j)
-			conn, err := net.Dial("tcp", add)
-
-			if err != nil {
-				return
-			}
-			conn.Close()
-			ans = append(ans, j)
-		}(i)
-	}
-	wg.Wait()
-	end := time.Since(start) / 1e9
-	fmt.Println(ans)
-	fmt.Printf("%d seconds", end)
 }
-*/
