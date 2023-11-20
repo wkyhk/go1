e1e7cb57e066d22d1d569657a65b32b33983df50
WHK <841065531@qq.com>
2023-11-20 14:00:58 +0800

    53 and 121

---
+++ leetcode/leetcode53.go
@@ -0,0 +1,19 @@
+package leetcode
+
+/*
+53. 最大子数组和
+给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+
+子数组 是数组中的一个连续部分。
+链接:https://leetcode.cn/problems/maximum-subarray/description/
+*/
+func maxSubArray(nums []int) int {
+	dp := make([]int, len(nums))
+	dp[0] = nums[0]
+	ans := dp[0]
+	for i := 1; i < len(nums); i++ {
+		dp[i] = max(dp[i-1]+nums[i], nums[i])
+		ans = max(ans, dp[i])
+	}
+	return ans
+}
