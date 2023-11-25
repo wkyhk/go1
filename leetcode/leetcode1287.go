489838d8129e52a7c9b9ccd4aa96c45d8b532624
WHK <841065531@qq.com>
2023-11-25 19:16:13 +0800

    1

--- leetcode/leetcode1287.go
+++ leetcode/leetcode1287.go
@@ -1,1 +1,21 @@
 package leetcode
+
+/*
+1287. 有序数组中出现次数超过25%的元素
+给你一个非递减的 有序 整数数组，已知这个数组中恰好有一个整数，它的出现次数超过数组元素总数的 25%。
+
+请你找到并返回这个整数
+链接：https://leetcode.cn/problems/element-appearing-more-than-25-in-sorted-array/description/
+*/
+func findSpecialInteger(arr []int) int {
+	m := make(map[int]int)
+	for i := 0; i < len(arr); i++ {
+		m[arr[i]]++
+	}
+	for k, v := range m {
+		if v > len(arr)/4 {
+			return k
+		}
+	}
+	return 0
+}
--- leetcode/leetcode2824.go
+++ leetcode/leetcode2824.go
@@ -1,1 +1,18 @@
 package leetcode
+
+/*
+2824. 统计和小于目标的下标对数目
+给你一个下标从 0 开始长度为 n 的整数数组 nums 和一个整数 target ，请你返回满足 0 <= i < j < n 且 nums[i] + nums[j] < target 的下标对 (i, j) 的数目。
+链接：https://leetcode.cn/problems/count-pairs-whose-sum-is-less-than-target/
+*/
+func countPairs(nums []int, target int) int {
+	ans := 0
+	for i := 0; i < len(nums)-1; i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] < target {
+				ans++
+			}
+		}
+	}
+	return ans
+}
